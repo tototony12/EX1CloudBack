b26b9b355e397f7b52ce5c115dbfc4119c4c7952
Antonio Ortiz <tototony@me.com>
2021-03-18 00:42:16 -0600

    app.js modificado

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const app = express();
 
+
+
+app.use(express.static(__dirname + '/public'))
+
+app.listen(3003, (err) => {
+    if (err) {
+      throw err
+    }
+    console.log('Servidor iniciado en el puerto 3000')
+  })
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+})
 
