594a0de8adce59669f79fc2ea3f225a13150e681
Antonio Ortiz <tototony@me.com>
2021-03-18 01:36:10 -0600

    index.js modificado

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
 
 
 app.use(express.static(__dirname + '/public'))
@@ -19,13 +20,6 @@ app.listen(3003, (err) => {
 app.post('/', (req, res) => {
     console.log(req.body)
 
-})
-
-
-const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
-const { IamAuthenticator } = require('ibm-watson/auth');
-
-function toneAnalyzer(analyzedText) {
     const toneAnalyzer = new ToneAnalyzerV3({
         version: '2017-09-21',
         authenticator: new IamAuthenticator({
@@ -33,19 +27,25 @@ function toneAnalyzer(analyzedText) {
         }),
         serviceUrl: 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/271700cf-6143-4817-8a24-ceb8420a2625',
       });
-      
-    const text = analyzedText;    
+
+    const text = req.body.text;    
 
     const toneParams = {
         toneInput: { 'text': text },
         contentType: 'application/json',
     };
-      
+
     toneAnalyzer.tone(toneParams)
-        .then(toneAnalysis => {
-          window.alert(JSON.stringify(toneAnalysis, null, 2));
-        })
-        .catch(err => {
-            window.alert('error:', err);
-        });
-}
+    .then(toneAnalysis => {
+        console.log(JSON.stringify(toneAnalysis, null, 2));
+    })
+    .catch(err => {
+        console.log('error:', err);
+    });
+
+})
+
+
+
+
+
