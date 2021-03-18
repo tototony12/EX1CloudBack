1bd507ff07c9d3eee785e749646c2f812b6c0cb3
Antonio Ortiz <tototony@me.com>
2021-03-17 23:49:22 -0600

    app.js modificado

--- index.js
+++ index.js
@@ -1,29 +1,2 @@
-const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
-const { IamAuthenticator } = require('ibm-watson/auth');
-require('dotenv').config();
 
-const toneAnalyzer = new ToneAnalyzerV3({
-  version: '2017-09-21',
-  authenticator: new IamAuthenticator({
-    apikey: process.env.API_KEY,
-  }),
-  serviceUrl: 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/271700cf-6143-4817-8a24-ceb8420a2625',
-});
 
-const text = 'Team, I know that times are tough! Product '
-  + 'sales have been disappointing for the past three '
-  + 'quarters. We have a competitive product, but we '
-  + 'need to do a better job of selling it!';
-
-const toneParams = {
-  toneInput: { 'text': text },
-  contentType: 'application/json',
-};
-
-toneAnalyzer.tone(toneParams)
-  .then(toneAnalysis => {
-    console.log(JSON.stringify(toneAnalysis, null, 2));
-  })
-  .catch(err => {
-    console.log('error:', err);
-  });
---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+require('dotenv').config();
+
+const textForm = document.querySelector('.formulario')
+let texto = document.getElementById('texto')
+
+textForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const toneAnalyzer = new ToneAnalyzerV3({
+        version: '2017-09-21',
+        authenticator: new IamAuthenticator({
+          apikey: process.env.API_KEY,
+        }),
+        serviceUrl: 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/271700cf-6143-4817-8a24-ceb8420a2625',
+      });
+      
+    const text = texto.value;
+      
+    const toneParams = {
+        toneInput: { 'text': text },
+        contentType: 'application/json',
+    };
+      
+    toneAnalyzer.tone(toneParams)
+        .then(toneAnalysis => {
+          console.log(JSON.stringify(toneAnalysis, null, 2));
+        })
+        .catch(err => {
+          console.log('error:', err);
+        });
+})
